e9d6bac188423dcb4c07932c5a508d75dc0f5e36
g-a-ankit <gaankit99@gmail.com>
2025-10-16 00:26:44 +0530

    feat: use winston for logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const isDev = process.env.NODE_ENV !== 'production';
+
+  winston.addColors({
+    error: 'bold red',
+    warn: 'yellow',
+    info: 'green',
+    debug: 'blue',
+    verbose: 'cyan',
+  });
+
+  const prodFormat = winston.format.combine(
+    winston.format.colorize({ all: true }),
+    winston.format.timestamp(),
+    winston.format.json(),
+  );
+
+  const logger = WinstonModule.createLogger({
+    // TODO: add other transport layers so that logs can be redirected to other sources like elsaticsearch, HDFS etc
+    transports: [
+      new winston.transports.Console({
+        level: isDev ? 'debug' : 'info',
+        format: prodFormat,
+      }),
+    ],
+  });
+
+  const app = await NestFactory.create(AppModule, {
+    // logger
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
