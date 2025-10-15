dfcbf0ae6a2cf10644579db44213b9956695c20b
g-a-ankit <gaankit99@gmail.com>
2025-10-16 00:24:04 +0530

    refac: move generic enum to common

---
+++ src/common/enum.ts
@@ -0,0 +1,15 @@
+export enum Status {
+  SUCCESS = 'SUCCESS',
+  SKIPPED = 'SKIPPED',
+  PENDING = 'PENDING',
+  IN_PROGRESS = 'IN_PROGRESS',
+  COMPLETED = 'COMPLETED',
+  FAILED = 'FAILED',
+  PARTIALLY_COMPLETED = 'PARTIALLY_COMPLETED',
+}
+
+export enum FileOperation {
+  CREATE = 'CREATE',
+  MODIFY = 'MODIFY',
+  DELETE = 'DELETE',
+}
